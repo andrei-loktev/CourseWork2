96d96bc4ac2b168c9bd1a45b454e32fc9ee353e6
andrei-loktev <119010210+andrei-loktev@users.noreply.github.com>
2023-08-21 23:49:47 +0300

    написан контроллер

---
+++ CourseWork2/src/main/java/com/example/CourseWork2/controller/ExamController.java
@@ -0,0 +1,24 @@
+package com.example.CourseWork2.controller;
+
+import com.example.CourseWork2.object.Question;
+import com.example.CourseWork2.service.ExaminerService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+@RestController
+@RequestMapping("/exam")
+public class ExamController {
+    private final ExaminerService examinerService;
+
+    public ExamController(ExaminerService examinerService) {
+        this.examinerService = examinerService;
+    }
+
+    @GetMapping("/get/{amount}")
+    public Collection<Question> getQuestions(@PathVariable("amount") int amount) {
+        return examinerService.getQuestions(amount);
+    }
+}
